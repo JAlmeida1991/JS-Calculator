ab529c0e8d1a97590ecf6a4f505d55688e8bffc7
Jonathan <jalmeida0291@gmail.com>
2018-08-24 21:11:52 -0400

    fixed opperator button ui on keypress

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ num.forEach(num => {
   num.addEventListener("click", function(e) {
     let key = this.textContent;
     preventMultipleZeroesBeforePeriod(key);
-    // This will check if user does not want to use sum for arithmetic
-    // Only opperators can be added to the array... Normal integers are not added
+    /* This will check if user does not want to use sum for arithmetic
+     Only opperators can be added to the array... Normal integers are not added */
     if (arr.length === 1 && arr[0] === sum) {
       arr = [];
       properNumber(key);
@@ -71,19 +71,15 @@ answer.addEventListener("click", function() {
   currentNum = "";
 });
 
-clear.addEventListener("click", function() {
-  init();
-});
+clear.addEventListener("click", init);
 
 document.addEventListener("keypress", function(e) {
   console.log(e.key);
-
+  let key = e.key;
   // KEY PRESS IS IN NUM ARRAY:
   if (keys.nums.indexOf(e.key) >= 0) {
     // this is set to the document object must use e.key instead of this.key
     // console.log(this);
-    let key = e.key;
-    console.log(key);
     buttonUI(key, btns);
     preventMultipleZeroesBeforePeriod(key);
     if (arr.length === 1 && arr[0] === sum) {
@@ -95,9 +91,9 @@ document.addEventListener("keypress", function(e) {
       displayScreen(currentNum);
     }
   }
-
   // KEY PRESS IS IN OPERATOR ARRAY:
   else if (keys.opperator.indexOf(e.key) >= 0) {
+    buttonUI(key, btns);
     addZeroToBeginning();
     opperator(e.key);
     displayScreen(e.key);
@@ -105,6 +101,7 @@ document.addEventListener("keypress", function(e) {
 
   // KEY PRESS IS IN EQUAL ARRAY
   else if (keys.equal.indexOf(e.key) >= 0) {
+    buttonUI(key, btns);
     if (!currentNum && arr.length === 0) {
       return;
     }
@@ -165,9 +162,9 @@ function init() {
   displayScreen("0");
 }
 
-// NEEDED IF USER CHOOSES TO START ARRAY WITH OPERATOR.
-// CANNOT INITIALLY SET ARRAY AT 0 SINCE EVAL WILL NOT PRODUCE DESIRED RESULTS
-// I.E. eval([0333].join("")) IS 219 NOT 333
+/*NEEDED IF USER CHOOSES TO START ARRAY WITH OPERATOR.
+CANNOT INITIALLY SET ARRAY AT 0 SINCE EVAL WILL NOT PRODUCE DESIRED RESULTS
+I.E. eval([0333].join("")) IS 219 NOT 333 */
 function addZeroToBeginning() {
   if (arr.length === 0 && currentNum === "") {
     arr.push(0);
@@ -185,22 +182,40 @@ function preventMultipleZeroesBeforePeriod(key) {
 function displayScreen(val) {
   screen.textContent = val;
 }
+
 function buttonUI(key, btns) {
+  // Need to call Array.from on btns since node lists do not have access to filter.
   const btnArr = Array.from(btns).filter(function(element) {
     return key === element.value;
   });
   // Destructuring in order to get value from btnArr
   const [btnKey] = btnArr;
-  // Key will look like it is being pressed
+  // If the key is not an opperator or equal, apply the following styles
   btnKey.style.transform = "translateY(.2rem)";
   btnKey.style.boxShadow = "0 0.2rem 0.2rem #333";
-  btnKey.style.color = "#fff";
-  btnKey.style.backgroundColor = "#333";
-  // after 1/4th a sceond, return orginal style
-  setTimeout(function() {
-    btnKey.style.transform = "translateY(0rem)";
-    btnKey.style.boxShadow = "none";
-    btnKey.style.color = "#000";
-    btnKey.style.backgroundColor = "#fff";
-  }, 250);
+
+  if (!keys.opperator.includes(key) && !keys.equal.includes(key)) {
+    // Key will look like it is being pressed
+    btnKey.style.color = "#fff";
+    btnKey.style.backgroundColor = "#333";
+    // after 1/4th a sceond, return orginal style
+    setTimeout(function() {
+      btnKey.style.transform = "translateY(0rem)";
+      btnKey.style.boxShadow = "none";
+      btnKey.style.color = "#000";
+      btnKey.style.backgroundColor = "#fff";
+    }, 250);
+    // Else apply the following styles for key if it is an opperator or equal
+  } else {
+    // Key will look like it is being pressed
+    btnKey.style.color = "#333";
+    btnKey.style.backgroundColor = "#f39c12";
+    // after 1/4th a sceond, return orginal style
+    setTimeout(function() {
+      btnKey.style.transform = "translateY(0rem)";
+      btnKey.style.boxShadow = "none";
+      // btnKey.style.color = "#e67e22";
+      btnKey.style.backgroundColor = "#e67e22";
+    }, 250);
+  }
 }
